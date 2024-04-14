cf1e5ee369d338cc05bf2d7e867945ef61610e9e
grupo4 <silvia2484@gmail.com>
2024-04-14 14:14:48 -0300

    Se corrigio videoclub y aeropuerto, se añadio paso a paso como se crearon las tablas y las relaciones

--- aeropuertos.sql
+++ aeropuertos.sql
@@ -1,4 +1,83 @@
+/*ejercicio 5*/
+
 use aeropuertos;
+
+/*tablas
+aeropurtos(@Id_Aeropuertos, Nombre_Aeropuerto, Ubicacion, Aeropuertoscol, @usoPistasId)
+aviones(@id_avion, tipo_de_avion, cantidaddeaviones, cantidaddeasientos, @Id_vuelos)
+checkin(@idCheckin, AeropuertosId, id_aerolinea, mostradorId)
+lineasaereas(@Id_Aerolinea, Nombre_aerolinea, AvionId)
+mostradores(ID_MOSTRADOR, numero, lugar_de_ubicacion, tipo_de_mostrador)
+pasajero(Nro_documento, Nombre, tipo_documento, Id_cliente)
+reserva(@ID_RESERVA, Id_cliente, Nro_de_vuelo, fecha_de_vencimiento)
+tipodepista(@id_tipodepista, tipo)
+usodepista(@id_pistausopista, @id_tipodepista, fecha_hora, @id_aeropuerto, id_avion)
+vuelos(@id_vuelos, Tipo_de_vuelos, Fecha_de_salida, Hora_de_salida, Duracion, ciudad_origen, ciudad_destino , @reservaId)
+*/
+CREATE TABLE aeropuertos (
+    Id_Aeropuertos INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre_aeropuerto VARCHAR(255),
+    Ubicacion VARCHAR(255),
+    Aeropuertoscol VARCHAR(15),
+    usoPistasId int
+);
+CREATE TABLE aviones (
+    id_avion INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_de_avion VARCHAR(255),
+    cantidad_de_aviones int,
+    Id_vuelos int
+);
+CREATE TABLE checkin (
+    idCheckin INT AUTO_INCREMENT PRIMARY KEY,
+    id_aerolinea int,
+    AeropuertosId int,
+    mostradorId int
+);
+CREATE TABLE lineasaereas (
+    Id_Aerolinea INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre_aerolinea VARCHAR(255),
+    AvionID int
+);
+CREATE TABLE mostradores (
+    ID_MOSTRADOR INT AUTO_INCREMENT PRIMARY KEY,
+    numero int,
+    lugar_de_ubicacion VARCHAR(255),
+    tipo_de_mostrador int
+);
+CREATE TABLE pasajero (
+    Id_Cliente INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(255),
+    tipo_de_documento varchar(15),
+    Nro_documento int
+);
+CREATE TABLE reserva (
+    ID_RESERVA INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente int,
+    Nro_de_vuelo int,
+   Fecha_de_vencimiento int
+);
+CREATE TABLE tipodepista (
+    id_tipodepista INT AUTO_INCREMENT PRIMARY KEY,
+    tipo VARCHAR(15)
+);
+CREATE TABLE usodepista (
+    id_pistausopista INT AUTO_INCREMENT PRIMARY KEY,
+    id_tipodepista int,
+    Fecha_hora int,
+    id_aeropuerto int,
+    id_avion int
+);
+CREATE TABLE vuelos (
+    id_vuelos INT AUTO_INCREMENT PRIMARY KEY,
+    Tipo_de_vuelo VARCHAR(255),
+    Fecha_de_salida int,
+    Hora_de_salida int, 
+    Duracion int,
+    ciudad_origen varchar(255),
+    ciudad_destino varchar(255),
+    reservaId int
+);
+/*ver tablas*/
 describe aeropuertos;
 describe aviones;
 describe checkin;
@@ -12,21 +91,17 @@ describe vuelos;
 
 /* las relaciones son:
 fk_aeropuertos_checkin*/
-/*tengo que añadir la fk en checkin*/
-Alter table checkin
-add AeropuertosID int;
+
 ALTER TABLE checkin
 ADD CONSTRAINT fk_aeropuertos_checkin
-FOREIGN KEY (AeropuertosID) REFERENCES aeropuertos(Id_Aeropuertos);
+FOREIGN KEY (AeropuertosId) REFERENCES aeropuertos(Id_Aeropuertos);
 
 /*fk_checkin_lineasaereas*/
 ALTER TABLE checkin
 ADD CONSTRAINT fk_checkin_lineasaereas
-FOREIGN KEY (AeropuertosID) REFERENCES lineasaereas(Id_Aerolinea);
+FOREIGN KEY (id_aerolinea) REFERENCES lineasaereas(Id_Aerolinea);
 
 /*fk_lineasaereas_aviones*/
-Alter table lineasaereas
-add AvionID int;
 ALTER TABLE lineasaereas
 ADD CONSTRAINT fk_lineasaereas_aviones
 FOREIGN KEY (AvionID) REFERENCES aviones(id_avion);
@@ -34,11 +109,9 @@ FOREIGN KEY (AvionID) REFERENCES aviones(id_avion);
 /*fk_aviones_vuelos*/
 ALTER TABLE aviones
 ADD CONSTRAINT fk_aviones_vuelos
-FOREIGN KEY (Id_Aerolineas) REFERENCES vuelos(id_vuelos);
+FOREIGN KEY (Id_vuelos) REFERENCES vuelos(id_vuelos);
 
 /*fk_vuelos_reserva*/
-alter table vuelos
-add reservaId int;
 alter table vuelos
 add constraint fk_vuelos_reserva
 foreign key (reservaId) references reserva(ID_RESERVA); 
@@ -46,11 +119,9 @@ foreign key (reservaId) references reserva(ID_RESERVA);
 /*fk_reserva_pasajero*/
 alter table reserva
 add constraint fk_reserva_pasajero
-foreign key (Id_cliente) references pasajero(Nro_documento);
-
+foreign key (Id_cliente) references pasajero(id_cliente);
+describe usodepista;
 /*fk_aeropuertos_usodepista*/
-alter table aeropuertos
-add usoPistasId int; 
 alter table aeropuertos
 add constraint fk_aeropuertos_usodepista
 foreign key (usoPistasId) references usodepista(id_pistausopista);
@@ -67,10 +138,5 @@ foreign key (id_tipodepista) references tipodepista(id_tipodepista);
 
 /*fk_checkin_mostradores*/
 alter table checkin
-add mostradorId int;
-alter table checkin
 add constraint fk_checkin_mostradores
 foreign key (mostradorId) references mostradores(ID_MOSTRADOR);
-describe checkin;
-alter table checkin
-drop id_mostrador;
--- videoclub.sql
+++ videoclub.sql
@@ -1,7 +1,38 @@
-/* use + nombre entidad, para trabajar*/
 use videoclub;
+/*creo las tablas:
+cliente(@idCliente, Nombre, Email, Num_celular)
+pelicula(@idPelicula, Genero, Año, Stock, @videoclub_id)
+proveedor(@idProveedor, Stock_de_peliculas, Datos_del_Proveedor)
+transaccion(@idTransaccion, Tipo, Fecha, @ClienteId)
+videoclub(@idVideoclub, Contacto, @Proveedor_Id)*/
+CREATE TABLE cliente (
+    idCliente INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(255),
+    Email VARCHAR(255),
+    Num_celular VARCHAR(15)
+);
+CREATE TABLE pelicula (
+    idPelicula INT AUTO_INCREMENT PRIMARY KEY,
+    Genero VARCHAR(255),
+    Año int,
+    Stock int
+);
+CREATE TABLE transaccion (
+    idTransaccion INT AUTO_INCREMENT PRIMARY KEY,
+    Tipo VARCHAR(255),
+    Fecha int
+);
+CREATE TABLE proveedor (
+    idProveedor INT AUTO_INCREMENT PRIMARY KEY,
+    Stock_de_peliculas int,
+    Datos_del_Proveedor VARCHAR(255)
+);
+CREATE TABLE videoclub (
+    idVideoclub INT AUTO_INCREMENT PRIMARY KEY,
+    Contacto VARCHAR(255)
+);
 
-/* describe + nombre tabla*/
+/*ver tablas: describe + nombre tabla*/
 describe cliente;
 describe pelicula;
 describe proveedor;
@@ -9,25 +40,22 @@ describe transaccion;
 describe videoclub;
 
 /* quitar o agregar variables ALTER TABLE nombre_de_tabla
-ADD nombre_de_variable tipo_de_dato;, lo uso par añadir las fk para realizar las relaciones */
+ADD nombre_de_variable tipo_de_dato;, lo uso par añadir las fk para realizar las relaciones 
+añado las variables que me sirven para las relaciones*/
 
 ALTER TABLE  pelicula
 ADD videoclub_Id int;
 
-ALTER TABLE  transacicion
+ALTER TABLE  transaccion
 ADD clienteId int;
 
 ALTER TABLE  videoclub
 ADD Proveedor_Id int;
 
-/* Genero las relaciones entre tablas: cliente-transaccion-pelicula// pelicula-videoclub-proveedor
-ALTER TABLE pedidos
-ADD CONSTRAINT fk_usuario
-FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario);
-*/
+/* Genero las relaciones entre tablas: cliente-transaccion-pelicula// pelicula-videoclub-proveedor*/
 ALTER TABLE transaccion
 ADD CONSTRAINT fk_cliente_transaccion
-FOREIGN KEY (cliente_Id) REFERENCES cliente(idCliente);
+FOREIGN KEY (clienteId) REFERENCES cliente(idCliente);
 
 ALTER TABLE transaccion
 ADD CONSTRAINT fk_transaccion_pelicula
@@ -35,16 +63,8 @@ FOREIGN KEY (clienteId) REFERENCES pelicula(idPelicula);
 
 ALTER TABLE pelicula
 ADD CONSTRAINT fk_pelicula_videoclub
-FOREIGN KEY (cliente_Id) REFERENCES videoclu(idVideoclub);
+FOREIGN KEY (videoclub_Id) REFERENCES videoclub(idVideoclub);
 
 ALTER TABLE videoclub
 ADD CONSTRAINT fk_videoclub_proveedor
-FOREIGN KEY (Proveedor_ID) REFERENCES cliente(idProveedor);
-
-/* para borrar la relacion fk que hice:
-ALTER TABLE pedidos
-DROP FOREIGN KEY fk_usuario;*/
-
-/* Ejemplo
-ALTER TABLE transaccion
-DROP foreign key fk_cliente_transaccion1;*/
+FOREIGN KEY (Proveedor_ID) REFERENCES proveedor(idProveedor);
