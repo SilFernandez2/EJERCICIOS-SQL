2dff81a2161238260f19dffbe21d62c5ad5835ab
Lautaroflores <90422530+Lautaroflores@users.noreply.github.com>
2024-05-08 20:23:15 -0300

    Ejercicio 12 terminado

--- aeropuertos/ejercicio12.sql
+++ aeropuertos/ejercicio12.sql
@@ -23,12 +23,13 @@ CREATE FUNCTION ContarAeropuertosPorUbicacion (
     ubicacion_parametro VARCHAR(255)
 )
 RETURNS INT
+-- Si no agrego las dos lineas de abajo me da error
 READS SQL DATA
 DETERMINISTIC
 BEGIN
     DECLARE cantidad_aeropuertos INT;
     
-    -- Contar la cantidad de aeropuertos en la ubicación especificada
+    -- Cuenta la cantidad de aeropuertos en la ubicación
     SELECT COUNT(*) INTO cantidad_aeropuertos FROM aeropuertos WHERE Ubicacion = ubicacion_parametro;
     
     RETURN cantidad_aeropuertos;
@@ -48,14 +49,15 @@ FOR EACH ROW
 BEGIN
     DECLARE message_text varchar(255);
     IF NEW.Nombre = 'Julian Assange' THEN
-        SIGNAL SQLSTATE '45000'
-        SET message_text = 'Julian Assange no debería estar acá.';
+        SIGNAL SQLSTATE '45000' --Excepción no controlada definida por el usuario.
+        SET message_text = 'Julian Assange no debería estar acá.'; --Esto sale por consola
     END IF;
 END //
 
 DELIMITER ;
+
 -- Pruebo que funcione todo ok 
-INSERT INTO pasajero(Nombre, tipo_de_documento, Nro_documento)VALUES ("Julian Assange", "DNI", "12346745");
+INSERT INTO pasajero(Nombre, tipo_de_documento, Nro_documento) VALUES ("Julian Assange", "DNI", "12346745");
 
 -- <----------TRIGGER-2------------> --
 DELIMITER //
